79140992473f7b36951124e994750e47f3b7dbfb
Oleg «Eleidan» Kulyk <helh-saintman@yandex.ru>
2015-02-06 05:40:07 +0200

    Update 'help' command to work properly.

--- lib/mimir/commands/help/cmd.rb
+++ lib/mimir/commands/help/cmd.rb
@@ -4,5 +4,8 @@ require 'mimir/options'
 
 usage_file = File.expand_path(File.join('usage.erb'), File.dirname(__FILE__))
 usage = Mimir::Usage.new(file: usage_file).render()
-puts options = Mimir::Options.new(usage).parse()
-puts usage if options.empty?
+options = Mimir::Options.new(usage).parse()
+# Get path to the command
+command = options['<command>']
+commands_usage_file = File.join(usage_file.split(File::SEPARATOR)[0..-3], command, 'usage.erb')
+puts usage = Mimir::Usage.new(file: commands_usage_file).render()
