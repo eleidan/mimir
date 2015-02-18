ad7da278d697299a440316583ad28b10f815139b
Oleg «Eleidan» Kulyk <helh-saintman@yandex.ru>
2015-02-18 22:50:27 +0200

    Integrate TravisCI.

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,5 +1,5 @@
 if ENV['CI']
-  ENV['CODECLIMATE_REPO_TOKEN'] = '8d6ebf9e62d59c7ac7d99e41d28e06f466e74ffa1ed23f9a7fea19306f9761dc'
+  # ENV['CODECLIMATE_REPO_TOKEN'] = '8d6ebf9e62d59c7ac7d99e41d28e06f466e74ffa1ed23f9a7fea19306f9761dc'
   require 'codeclimate-test-reporter'
   CodeClimate::TestReporter.start
 else
