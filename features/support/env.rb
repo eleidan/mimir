40fdc36f9027f02c288bcf2b87b3486548b50ad9
Oleg «Eleidan» Kulyk <helh-saintman@yandex.ru>
2015-02-18 22:26:31 +0200

    Fix Code Climate integration.

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,12 +1,11 @@
 if ENV['CI']
-  ENV['CODECLIMATE_REPO_TOKEN'] = '8d6ebf9e62d59c7ac7d99e41d28e06f466e74ffa1ed23f9a7fea19306f9761dc bundle exec rake'
+  ENV['CODECLIMATE_REPO_TOKEN'] = '8d6ebf9e62d59c7ac7d99e41d28e06f466e74ffa1ed23f9a7fea19306f9761dc'
   require 'codeclimate-test-reporter'
   CodeClimate::TestReporter.start
 else
   require 'simplecov'
   SimpleCov.start
 end
-
 
 $LOAD_PATH.unshift(File.dirname(__FILE__) + '/../../lib')
 require 'mimir'
