8012a93b89e4b6a1f1237979c1d2259222a763fa
Oleg «Eleidan» Kulyk <helh-saintman@yandex.ru>
2015-02-21 02:45:32 +0200

    Update the 'dump' command to get more features.

--- lib/mimir/commands/dump/cmd.rb
+++ lib/mimir/commands/dump/cmd.rb
@@ -6,22 +6,14 @@ def command(opts)
   defaults  = {'--format' => 'plain'}
   options   = defaults.merge(opts)
   the_class = Mimir::Options::get_option(options,'<class>').strip
-  # the_format  = Mimir::Options::get_option(options,'--format').downcase
-  # Refactoring TODO: extract method for lines below
-  methods = {}
-  tmp = get_class_public_methods(the_class)
-  methods['public methods']     = tmp if tmp.size > 0
-  tmp = get_class_protected_methods(the_class)
-  methods['protected methods']  = tmp if tmp.size > 0
-  tmp = get_class_private_methods(the_class)
-  methods['private methods']    = tmp if tmp.size > 0
-  # Refactoring TODO: extract method for lines below
-  result  = {}
-  result.store(the_class, methods)
-  # Inject data
+  result  = {
+    the_class => {
+      # 'constants' => Hash.new,
+      'methods'   => get_methods(the_class),
+      # 'values'    => Hash.new
+    }
+  }
   options[:data] = result
-  # puts options[:data]
-  # output  = Mimir::View::Result.new(options).render()
-  # puts output
-  return options
+
+  options
 end
--- lib/mimir/services/help.rb
+++ lib/mimir/services/help.rb
@@ -3,20 +3,6 @@ module Mimir
     require 'set'
     SUSPECTED = '/\\[]{}~`!@#$%^&*()_+,.;\'"?'.chars.to_set
 
-    def get_class_public_methods(cls)
-      return [] unless valid?(cls)
-      eval(cls).public_methods(false).sort
-    end
-
-    def get_class_protected_methods(cls)
-      return [] unless valid?(cls)
-      eval(cls).protected_methods(false).sort
-    end
-
-    def get_class_private_methods(cls)
-      return [] unless valid?(cls)
-      eval(cls).private_methods(false).sort
-    end
 
     def valid?(input)
       if input.chars.to_set.intersect?(SUSPECTED)
@@ -27,5 +13,72 @@ module Mimir
       end
     end
 
+    # TODO: remove?
+    def remove_empty_items(arg)
+      arg.reject! {|k,v| v.empty?}
+    end
+
+    # ************************************************************************
+    # Class related methods
+    def get_methods(cls)
+      return {} unless valid?(cls)
+      puts obj = get_instance(cls)
+      result = {
+        'public'  => {
+          'class'     => get_class_public_methods(cls),
+          'instance'  => get_instance_public_methods(obj)
+        },
+        'protected'  => {
+          'class'     => get_class_protected_methods(cls),
+          'instance'  => get_instance_protected_methods(obj)
+        },
+        'private'  => {
+          'class'     => get_class_private_methods(cls),
+          'instance'  => get_instance_private_methods(obj)
+        }
+      }
+
+      result
+    end
+
+    def get_class_public_methods(cls)
+      eval(cls).public_methods(false).sort
+    end
+
+    def get_class_protected_methods(cls)
+      eval(cls).protected_methods(false).sort
+    end
+
+    def get_class_private_methods(cls)
+      eval(cls).private_methods(false).sort
+    end
+
+    # ************************************************************************
+    # Instance related methods
+
+    def get_instance_public_methods(obj)
+      return [] if obj.nil?
+      obj.public_methods(false).sort
+    end
+
+    def get_instance_protected_methods(obj)
+      return [] if obj.nil?
+      obj.protected_methods(false).sort
+    end
+
+    def get_instance_private_methods(obj)
+      return [] if obj.nil?
+      obj.private_methods(false).sort
+    end
+
+    def has_new?(cls)
+      Object::const_get(cls).respond_to?(:new)
+    end
+
+    def get_instance(cls)
+      return 1 if cls == 'Fixnum'
+      return nil unless has_new?(cls)
+      Object::const_get(cls).new
+    end
   end
 end
