f16180c06da6e9f16a7498c8b783c41937f7e78b
Oleg «Eleidan» Kulyk <helh.saintman@gmail.com>
2015-03-18 19:26:56 +0200

    Remove the 'common' command from the list of available ones, and fix the 'help' command.

--- lib/mimir/commands/help/cmd.rb
+++ lib/mimir/commands/help/cmd.rb
@@ -1,9 +1,9 @@
 def command(options)
   usage_file = File.expand_path(File.join('usage.erb'), File.dirname(__FILE__))
-  usage = Mimir::View::Usage.new(file: usage_file).render()
+  usage = Mimir::Views::Usage.new(file: usage_file).render()
   command_name = options['<command>']
   # Get path to the command
   commands_usage_file = File.join(usage_file.split(File::SEPARATOR)[0..-3], command_name, 'usage.erb')
-  puts usage = Mimir::View::Usage.new(file: commands_usage_file, command: command_name).render()
+  puts usage = Mimir::Views::Usage.new(file: commands_usage_file, command: command_name).render()
   return Hash.new
 end
